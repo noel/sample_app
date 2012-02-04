ff0b134bb93fec7b6d69ea61d3cfa13e91acb113
Noel Gomez <noel_gomez@yahoo.com>
2012-02-03 22:04:08 -0800

    remove see and assets precompile for blueprint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ SampleApp::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, 
   # and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Precompile Blueprint CSS
-  config.assets.precompile += %w[ blueprint/*.css ]
+  # config.assets.precompile += %w[ blueprint/*.css ]
